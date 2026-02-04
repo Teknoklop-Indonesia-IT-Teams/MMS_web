c7b50c19e5f25b64c9ffd07dcb5242f4046077e0
Nurus Laily Aprilia <54704667+nuruslaily@users.noreply.github.com>
2026-02-04 13:10:12 +0700

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://api-mms.teknoklop.com",
         changeOrigin: true,
       },
     },
