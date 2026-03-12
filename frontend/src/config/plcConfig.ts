f70058a982b14fc5f7a5e091dca8355460adfe58
Nurus Laily Aprilia <54704667+nuruslaily@users.noreply.github.com>
2026-03-12 11:30:56 +0700

    update type

--- frontend/src/config/plcConfig.ts
+++ frontend/src/config/plcConfig.ts
@@ -1,4 +1,13 @@
-import { Cpu, CircuitBoard, Settings, Server } from "lucide-react";
+import {
+  Cpu,
+  CircuitBoard,
+  Settings,
+  Server,
+  Microchip,
+  BrainCircuit,
+  MemoryStick,
+  Gpu,
+} from "lucide-react";
 
 export const PLC_CONFIG: Record<string, { icon: any; color: string }> = {
   // PLC
@@ -7,7 +16,7 @@ export const PLC_CONFIG: Record<string, { icon: any; color: string }> = {
     color: "from-indigo-500 to-indigo-600",
   },
   Omron: {
-    icon: CircuitBoard,
+    icon: Microchip,
     color: "from-green-500 to-green-600",
   },
   Mitsubishi: {
@@ -15,11 +24,27 @@ export const PLC_CONFIG: Record<string, { icon: any; color: string }> = {
     color: "from-red-500 to-red-600",
   },
   Schneider: {
-    icon: Settings,
+    icon: MemoryStick,
     color: "from-purple-500 to-purple-600",
   },
   AllenBradley: {
     icon: Server,
     color: "from-orange-500 to-orange-600",
+  },
+  "Flexem FL7": {
+    icon: CircuitBoard,
+    color: "from-blue-500 to-blue-600",
+  },
+  "Flexem FL8": {
+    icon: BrainCircuit,
+    color: "from-emerald-500 to-emerald-600",
+  },
+  "Flexem FL6": {
+    icon: Gpu,
+    color: "from-amber-500 to-amber-600",
+  },
+  "Flexem FL5": {
+    icon: CircuitBoard,
+    color: "from-cyan-500 to-cyan-600",
   },
 };
